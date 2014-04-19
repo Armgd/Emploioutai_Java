1113675e8deb229edd6fdda13f5dc03e51cb0451
Nico-port <nicolas.gras@live.fr>
2014-04-19 20:29:07 +0200

    Maps --> récupération et affichage de la position GPS

--- app/src/main/java/com/aup/emploioutai/app/MapsActivity.java
+++ app/src/main/java/com/aup/emploioutai/app/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.aup.emploioutai.app;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -17,6 +22,11 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        // Use the LocationManager class to obtain GPS locations
+        LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        LocationListener mlocListener = new MyLocationListener();
+        mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
     }
 
     @Override
@@ -60,6 +70,45 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+    }
+    /**
+     * Implémentation de la classe  MylocationListener pour récupérer la position GPS du téléphone
+     */
+    private class MyLocationListener implements LocationListener {
+
+        //Récupère la position GPS
+        @Override
+        public void onLocationChanged(Location loc){
+            loc.getLatitude();
+            loc.getLongitude();
+            String Text = "My current location is: " +
+                    "Lat= " + loc.getLatitude()+ "Long = " + loc.getLongitude();
+            Toast.makeText(getApplicationContext(),
+                    Text,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        //GPS désactivé
+        @Override
+        public void onProviderDisabled(String provider){
+            Toast.makeText( getApplicationContext(),
+                    "Gps Disabled",
+                    Toast.LENGTH_SHORT ).show();
+
+        }
+
+        //GPS activé
+        @Override
+        public void onProviderEnabled(String provider){
+            Toast.makeText( getApplicationContext(),
+                    "Gps Enabled",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras){
+
+        }
     }
 }
